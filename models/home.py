3ab900a6f2627e486ad372b90c506090298d80c4
keniaescamilla <kenyaescamill@gmail.com>
2023-10-07 13:05:05 -0600

    kenia escamilla sm42

---
+++ models/home.py
@@ -0,0 +1,11 @@
+from flet_mvc import data
+from .index import IndexModel
+import flet as ft
+
+
+
+class HomeModel(IndexModel):
+    @data
+    def example_content(self):
+
+        return "\n\n\tinicio"
---
+++ models/settings.py
@@ -0,0 +1,9 @@
+from flet_mvc import FletModel, data
+import flet as ft
+
+
+class SettingsModel(FletModel):
+    @data
+    def example_content(self):
+        
+        return "\n\n\tAjustes"
